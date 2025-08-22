9b17d4c888a6b5f5c939d68d0bec17e8628f907c
Ilyasov Daniel <danielilyasov98@gmail.com>
2025-08-22 14:22:13 +0700

    работает

--- bot/cmd/bot/main.go
+++ bot/cmd/bot/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
-func main(){
-	fmt.Println("hello")
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("8407259143:AAE0AOt2CM1gax-dVAZ0-o0cpDr9S7z6jF8")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+  update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/IlyasovDaniel/Telegram_bot_v0.0.1
 
 go 1.21.4
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
