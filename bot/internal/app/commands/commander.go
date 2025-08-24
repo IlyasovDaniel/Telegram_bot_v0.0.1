dfb53d6f7e65c3138ecadf91c2c332afa4ffe0dc
Ilyasov Daniel <danielilyasov98@gmail.com>
2025-08-24 12:26:12 +0700

    распределил все функции по файлам

--- bot/internal/app/commands/commander.go
+++ bot/internal/app/commands/commander.go
@@ -1,8 +1,6 @@
 package commands
 
 import (
-	"log"
-
 	"github.com/IlyasovDaniel/Telegram_bot_v0.0.1/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -20,31 +18,4 @@ func NewCommander(
 		bot:            bot,
 		productService: productService,
 	}
-}
-
-func (c *Commander) Help(inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help - help\n"+
-			"/list - list products")
-	c.bot.Send(msg)
-}
-func (c *Commander) List(inputMessage *tgbotapi.Message) {
-	outputMgsText := "Here all the products: \n\n"
-
-	products := c.productService.List()
-	for _, p := range products {
-		outputMgsText += p.Title
-		outputMgsText += "\n"
-	}
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMgsText)
-	c.bot.Send(msg)
-}
-
-func (c *Commander) Default(inputMessage *tgbotapi.Message) {
-	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
-
-	c.bot.Send(msg)
 }
---
+++ bot/internal/app/commands/list.go
@@ -0,0 +1,18 @@
+package commands
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (c *Commander) List(inputMessage *tgbotapi.Message) {
+	outputMgsText := "Here all the products: \n\n"
+
+	products := c.productService.List()
+	for _, p := range products {
+		outputMgsText += p.Title
+		outputMgsText += "\n"
+	}
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMgsText)
+	c.bot.Send(msg)
+}
